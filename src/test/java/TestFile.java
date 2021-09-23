0b16ded2f60bc1038fcf43da5d662accf2be943e
liyutian <liyutian>
2021-09-23 18:55:30 +0800

    添加jacoco测试依赖环境

---
+++ src/test/java/TestFile.java
@@ -0,0 +1,40 @@
+public class TestFile {
+    public static void main(String[] args) {
+        int i = 1;
+        double j = 0;
+        long f;
+        switch (i) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            default:
+                break;
+        }
+        switch (i) {
+            case 0:
+                break;
+            case 1:
+                break;
+            default:
+                break;
+        }
+        if (i < 0) {
+            if (i < -1) {
+            } else {
+            }
+        } else if (i > 0) {
+            if (i > 2) {
+            } else if (i == 2) {
+            } else if (i > 1) {
+            } else {
+            }
+        } else {
+            if (j != 0) {
+            } else {
+            }
+        }
+    }
+}
